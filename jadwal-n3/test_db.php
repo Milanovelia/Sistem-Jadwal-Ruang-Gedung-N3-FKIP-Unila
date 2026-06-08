e44aaeaa4835ce86c31cf8d1f9873a202db5252a
Milanovelia <nvlyamila@gmail.com>
2026-06-08 07:39:15 +0700

    Delete jadwal-n3 directory

--- jadwal-n3/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'koneksi.php';
-$stmt = $pdo->query("SELECT * FROM ruangan");
-$ruangan = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode(["status" => "sukses", "ruangan" => $ruangan]);
-?>
